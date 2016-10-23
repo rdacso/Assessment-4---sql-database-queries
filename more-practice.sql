d74361e13282882922cd121a30ace6d442715586
vagrant <vagrant@vagrant.vm>
2016-10-23 20:14:45 +0000

    completed more-practice.sql. completed most of answers.sql

---
+++ more-practice.sql
@@ -0,0 +1,22 @@
+-- Include your solutions to the More Practice problems in this file.
+
+
+
+-- INSERT
+INSERT INTO models (year, brand_name, name) VALUES ('2015', 'Chevrolet', 'Malibu');
+INSERT 0 1
+INSERT INTO models (year, brand_name, name) VALUES ('2015', 'Suburu', 'Outback');
+INSERT 0 1
+
+-- CREATE TABLE
+CREATE TABLE awards(
+name VARCHAR(20) NOT NULL,
+year INTEGER NOT NULL,
+winner_id INTEGER NOT NULL);       
+
+
+-- More INSERT
+INSERT INTO awards (name, year, winner_id) VALUES('IIHS Safety Award', 2015, 49);
+INSERT 0 1
+INSERT INTO awards (name, year, winner_id) VALUES('IIHS Safety Award', 2015, 50);
+INSERT 0 1
